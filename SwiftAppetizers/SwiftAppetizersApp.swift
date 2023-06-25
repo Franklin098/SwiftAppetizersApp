070e8addbc4ef88a61dbaef4265eafdb51d019f0
Franklin Velasquez <iefranklin@hotmail.es>
2023-06-25 16:47:26 -0600

    Initial Commit

---
+++ SwiftAppetizers/SwiftAppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppetizersApp.swift
+//  SwiftAppetizers
+//
+//  Created by Franklin Velásquez on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
