77d1fa01b8d7d73defd0091158c6e2137fb328d1
franklin-telus <117105412+franklin-telus@users.noreply.github.com>
2023-07-13 22:11:17 -0600

    feat: add form validation, environment state to store order details, update readme

---
+++ SwiftAppetizers/Models/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  SwiftAppetizers
+//
+//  Created by Franklin Velásquez on 11/07/23.
+//
+
+import Foundation
---
+++ SwiftAppetizers/Views/Empty States/EmptyListView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyListView.swift
+//  SwiftAppetizers
+//
+//  Created by Franklin Velásquez on 10/07/23.
+//
+
+import SwiftUI
+
+struct EmptyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmptyListView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyListView()
+    }
+}
